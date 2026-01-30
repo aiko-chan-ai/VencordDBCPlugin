6bf857c47349ce82679693ec6d8833328655c10b
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-01-30 15:35:47 +0700

    feat: remove hardcoded

--- components/AuthBoxTokenLogin.tsx
+++ components/AuthBoxTokenLogin.tsx
@@ -5,76 +5,11 @@
  */
 
 import { Margins } from "@utils/margins";
-import { findByPropsLazy } from "@webpack";
+import { findByPropsLazy, findCssClassesLazy } from "@webpack";
 import { useState } from "@webpack/common";
 
-/** I hate Discord
- * v479219:
-        28731: function(e, t, n) {
-            "use strict";
-            e.exports = {
-                discordLogo: "discordLogo__921c5",
-                authBox: "authBox__921c5",
-                authBoxExpanded: "authBoxExpanded__921c5 authBox__921c5",
-                centeringWrapper: "centeringWrapper__921c5",
-                title: "title__921c5",
-                subText: "subText__921c5",
-                pill: "pill__921c5",
-                pillOnline: "pillOnline__921c5",
-                pillIcon: "pillIcon__921c5",
-                pillIconTotal: "pillIconTotal__921c5 pillIcon__921c5",
-                pillIconOnline: "pillIconOnline__921c5 pillIcon__921c5",
-                pillFlat: "pillFlat__921c5",
-                joiningAs: "joiningAs__921c5",
-                joiningAsAvatar: "joiningAsAvatar__921c5",
-                joiningAsUsername: "joiningAsUsername__921c5",
-                spinnerVideo: "spinnerVideo__921c5",
-                image: "image__921c5",
-                block: "block__921c5",
-                button: "button__921c5",
-                linkButton: "linkButton__921c5",
-                inviteIcon: "inviteIcon__921c5",
-                inviteLargeIcon: "inviteLargeIcon__921c5 inviteIcon__921c5",
-                downloadButtonSubtext: "downloadButtonSubtext__921c5"
-            }
-        },
- * v490370
-        380172(e, t, n) {
-            "use strict";
-            e.exports = {
-                ie: "discordLogo__921c5",
-                sL: "authBox__921c5",
-                PR: "authBoxExpanded__921c5 authBox__921c5",
-                f4: "centeringWrapper__921c5",
-                DD: "title__921c5",
-                Sv: "subText__921c5",
-                Io: "pill__921c5",
-                L1: "pillOnline__921c5",
-                nW: "pillIcon__921c5",
-                jk: "pillIconTotal__921c5 pillIcon__921c5",
-                _o: "pillIconOnline__921c5 pillIcon__921c5",
-                Z6: "pillFlat__921c5",
-                l1: "joiningAs__921c5",
-                yj: "joiningAsAvatar__921c5",
-                pp: "joiningAsUsername__921c5",
-                $$: "spinnerVideo__921c5",
-                Sl: "image__921c5",
-                om: "block__921c5",
-                TP: "inviteIcon__921c5",
-                yt: "inviteLargeIcon__921c5 inviteIcon__921c5",
-                UM: "downloadButtonSubtext__921c5"
-            }
-        },
- * i need hardcoded keys here because the class names are obfuscated and change often
- */
-export const authBoxModule = {
-    authBox: "authBox__921c5",
-    authBoxExpanded: "authBoxExpanded__921c5 authBox__921c5",
-    block: "block__921c5",
-    button: "button__921c5",
-};
-// findByPropsLazy("authBox", "authBoxExpanded", "block", "button");
-// v490370: Tested OK
+export const authBoxModule = findCssClassesLazy("authBox", "authBoxExpanded", "block", "button");
+
 export const titleModule = findByPropsLazy(
     "h5",
     "errorMessage",
@@ -83,90 +18,10 @@ export const titleModule = findByPropsLazy(
     "errorMessage",
     "errorSeparator",
 );
-// v490370: Tested OK
+
 export const inputModule = findByPropsLazy("inputWrapper", "inputDefault", "inputError");
 
-/**
- * v479219:
-        973013: function(e, t, n) {
-            "use strict";
-            e.exports = {
-                button: "button__201d5",
-                contents: "contents__201d5",
-                lookFilled: "lookFilled__201d5",
-                colorBrand: "colorBrand__201d5",
-                spinnerItem: "spinnerItem__201d5",
-                colorBrandInverted: "colorBrandInverted__201d5",
-                lookOutlined: "lookOutlined__201d5",
-                lookLink: "lookLink__201d5 " + n(546740).lowSaturationUnderline,
-                colorPrimary: "colorPrimary__201d5",
-                colorLink: "colorLink__201d5",
-                colorWhite: "colorWhite__201d5",
-                colorRed: "colorRed__201d5",
-                colorGreen: "colorGreen__201d5",
-                colorTransparent: "colorTransparent__201d5",
-                lookBlank: "lookBlank__201d5",
-                sizeTiny: "sizeTiny__201d5",
-                sizeSmall: "sizeSmall__201d5",
-                sizeMedium: "sizeMedium__201d5",
-                sizeLarge: "sizeLarge__201d5",
-                sizeMin: "sizeMin__201d5",
-                sizeMax: "sizeMax__201d5",
-                sizeIcon: "sizeIcon__201d5",
-                grow: "grow__201d5",
-                fullWidth: "fullWidth__201d5",
-                submitting: "submitting__201d5",
-                spinner: "spinner__201d5",
-                disabledButtonWrapper: "disabledButtonWrapper__201d5",
-                disabledButtonOverlay: "disabledButtonOverlay__201d5"
-            }
-        },
- * v490370:
-        134112(e, t, n) {
-            "use strict";
-            e.exports = {
-                x6: "button__201d5",
-                PG: "contents__201d5",
-                WL: "lookFilled__201d5",
-                x8: "colorBrand__201d5",
-                $N: "spinnerItem__201d5",
-                Qn: "colorBrandInverted__201d5",
-                uu: "lookOutlined__201d5",
-                M_: "lookLink__201d5 " + n(423367).xf,
-                cG: "colorPrimary__201d5",
-                I5: "colorLink__201d5",
-                bD: "colorWhite__201d5",
-                D: "colorRed__201d5",
-                RH: "colorGreen__201d5",
-                Ey: "colorTransparent__201d5",
-                Ev: "lookBlank__201d5",
-                Ei: "sizeTiny__201d5",
-                g4: "sizeSmall__201d5",
-                $g: "sizeMedium__201d5",
-                Pu: "sizeLarge__201d5",
-                CM: "sizeMin__201d5",
-                Gn: "sizeMax__201d5",
-                Rk: "sizeIcon__201d5",
-                wS: "grow__201d5",
-                Ij: "fullWidth__201d5",
-                B2: "submitting__201d5",
-                u1: "spinner__201d5",
-                Yr: "disabledButtonWrapper__201d5",
-                p5: "disabledButtonOverlay__201d5"
-            }
-        },
- */
-export const contentModule = {
-    button: "button__201d5",
-    contents: "contents__201d5",
-    lookFilled: "lookFilled__201d5",
-    colorBrand: "colorBrand__201d5",
-    sizeLarge: "sizeLarge__201d5",
-    fullWidth: "fullWidth__201d5",
-    grow: "grow__201d5",
-};
-/*
-findByPropsLazy(
+export const contentModule = findCssClassesLazy(
     "button",
     "lookFilled",
     "colorBrand",
@@ -175,7 +30,7 @@ findByPropsLazy(
     "grow",
     "contents",
 );
-*/
+
 const LoginToken = findByPropsLazy("loginToken", "login");
 
 export default function AuthBoxTokenLogin() {
